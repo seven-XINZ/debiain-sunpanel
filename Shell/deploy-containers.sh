ea770ae870e1106cb31637fa142e9cc0f346bf3a
seven-xinz <114560460+seven-XINZ@users.noreply.github.com>
2024-08-08 21:15:45 +0800

    Update deploy-containers.sh

--- Shell/deploy-containers.sh
+++ Shell/deploy-containers.sh
@@ -6,12 +6,7 @@ COMPOSE_DIR="$WORK_DIR/docker-compose"
 
 # 容器配置，格式为 容器名称=下载地址
 declare -A containers=(
-    [ddns-go]="https://gitee.com/kekylin/Debian-HomeNAS/raw/main/Docker%20Compose/ddns-go.yaml"
-    [dockge]="https://gitee.com/kekylin/Debian-HomeNAS/raw/main/Docker%20Compose/dockge.yaml"
-    [nginx-ui]="https://gitee.com/kekylin/Debian-HomeNAS/raw/main/Docker%20Compose/nginx-ui.yaml"
-    [portainer]="https://gitee.com/kekylin/Debian-HomeNAS/raw/main/Docker%20Compose/portainer.yaml"
-    [portainer_zh-cn]="https://gitee.com/kekylin/Debian-HomeNAS/raw/main/Docker%20Compose/portainer_zh-cn.yaml"
-    [scrutiny]="https://gitee.com/kekylin/Debian-HomeNAS/raw/main/Docker%20Compose/scrutiny.yaml"
+    [sun-panel]="https://github.com/seven-XINZ/debiain-sunpanel/blob/main/Docker%20Compose/sun-panel.yaml"
 )
 
 # 准备工作：检查并创建目录
@@ -84,5 +79,8 @@ main() {
     prepare_directory
     show_menu
 }
+
+main
+
 
 main
